20ff4ce3b3811997ddb62b24bad226b925d5abe0
ngrhadi <adinugraha.ista@yahoo.com>
2022-11-13 15:31:27 +0700

    setup

--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -1,8 +1,8 @@
-const ui = require('@company/ui/tailwind')
+const ui = require('@ngrhadi/ui/tailwind')
 
 module.exports = {
   presets: [require('@vercel/examples-ui/tailwind'), ui],
-  // `ui.content` includes a path to the components that are using tailwind in @company/ui
+  // `ui.content` includes a path to the components that are using tailwind in @ngrhadi/ui
   content: ui.content.concat([
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
